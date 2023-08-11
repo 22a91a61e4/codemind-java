6a3bfa11b124a4215ce39e2bdefbd79b8374f3b9
22a91a61e4 <119914648+22a91a61e4@users.noreply.github.com>
2023-08-11 11:41:27 +0530

    import java.util.Scanner;    public class sample2{     public static void main(String[] args){
             Scanner sc = new Scanner(System.in);
            String octalString = sc.next();
            System.out.println(Integer.parseInt(octalString,8));
            
        }
    
        }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+   public class sample2{
+    public static void main(String[] args){
+        
+         Scanner sc = new Scanner(System.in);
+        String octalString = sc.next();
+        System.out.println(Integer.parseInt(octalString,8));
+        
+    }
+
+    }
